8e3619547239965ad3583855859d815687515d3e
MaksymilianBilski <biger9@o2.pl>
2023-01-08 19:59:55 +0100

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,14 +4,6 @@ import { Section } from './Section/Section';
 import React, { Component } from 'react';
 
 export class App extends Component {
-  // constructor() {
-  //   super()
-  //   this.state = {
-  //     good: 0,
-  //     neutral: 0,
-  //     bad: 0
-  //   };
-  // }
   state = {
     good: 0,
     neutral: 0,
